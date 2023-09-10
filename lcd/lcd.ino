8831b00323a8cd3d0ea9c85850793fd9b441ef22
agos <agosnoriega2018@gmail.com>
2023-09-10 14:57:01 -0300

    Subiendo codigo de lcd

---
+++ lcd/lcd.ino
@@ -0,0 +1,22 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+//Crear el objeto lcd  dirección  0x3F y 16 columnas x 2 filas
+LiquidCrystal_I2C lcd(0x27,20,2);  //
+
+void setup() {
+  // Inicializar el LCD
+  lcd.init();
+  
+  //Encender la luz de fondo.
+  lcd.backlight();
+  
+  // Escribimos el Mensaje en el LCD.
+  lcd.print("Hola wuachi");
+}
+
+void loop() {
+   // Ubicamos el cursor en la primera posición(columna:0) de la segunda línea(fila:1)
+ 
+}
+
